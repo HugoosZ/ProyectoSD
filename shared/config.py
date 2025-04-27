97fce59af2ae29d4a8c352780a7a53a45c20d0d1
vertiass <tomas.leon1@mail.udp.cl>
2025-04-27 17:20:56 -0400

    ajustes

--- shared/config.py
+++
@@ -1,4 +0,0 @@
-# shared/config.py
-#MONGO_URI="mongodb+srv://waze_user:qwnuC6FRFUXsqTAw@wazeevents.3ykhrti.mongodb.net/?retryWrites=true&w=majority&appName=WazeEvents"
-#DB_NAME = "waze_traffic"
-#COLLECTION_NAME = "events"
